02fb24132cf7392a43abcb6aa6c4d0e3a9cfa514
himarkussohn <jmarkus.dev@gmail.com>
2018-11-28 23:28:48 +0200

    initial commit

---
+++ web-dump/src/main/java/com/himarkussohn/minddump/web/MindDumpApplication.java
@@ -0,0 +1,16 @@
+package com.himarkussohn.minddump.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author himarkussohn
+ * @since 2018-11-28
+ */
+@SpringBootApplication
+public class MindDumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindDumpApplication.class, args);
+    }
+}
